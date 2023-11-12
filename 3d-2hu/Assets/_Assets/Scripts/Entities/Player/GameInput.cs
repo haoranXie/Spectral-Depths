f7ace098cf0f4f963f65eadd295f793f1df13f1a
HaoranX39 <haoranxie2005@gmail.com>
2023-11-11 23:42:34 -0500

    Input Rebind System for UI

--- 3d-2hu/Assets/_Assets/Scripts/Entities/Player/GameInput.cs
+++ 3d-2hu/Assets/_Assets/Scripts/Entities/Player/GameInput.cs
@@ -18,6 +18,7 @@ public class GameInput : MonoBehaviour
 
     private PlayerInputActions playerInputActions;
     public event EventHandler OnPauseAction;
+    public event EventHandler OnBindingRebind;
     public static GameInput Instance {get;private set;}
 
     private void Awake(){
@@ -43,5 +44,57 @@ public class GameInput : MonoBehaviour
     public Vector2 GetMovementVectorNormalized(){
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>(); //Auto Normalized
         return inputVector;
+    }
+
+    public string GetBindingText(Binding binding){
+        switch(binding){
+            default:
+            case Binding.Move_Up:
+                return playerInputActions.Player.Move.bindings[1].ToDisplayString();
+            case Binding.Move_Down:
+                return playerInputActions.Player.Move.bindings[2].ToDisplayString();
+            case Binding.Move_Left:
+                return playerInputActions.Player.Move.bindings[3].ToDisplayString();
+            case Binding.Move_Right:
+                return playerInputActions.Player.Move.bindings[4].ToDisplayString();
+        }
+    }
+
+    public void RebindBinding(Binding binding, Action onActionRebound){
+        playerInputActions.Player.Disable();
+        InputAction inputAction;
+        int bindingIndex;
+
+        switch(binding){
+            default:
+            case Binding.Move_Up:
+                inputAction = playerInputActions.Player.Move;
+                bindingIndex=1;
+                break;
+            case Binding.Move_Down:
+                inputAction = playerInputActions.Player.Move;
+                bindingIndex=2;
+                break;
+            case Binding.Move_Left:
+                inputAction = playerInputActions.Player.Move;
+                bindingIndex=3;
+                break;
+            case Binding.Move_Right:
+                inputAction = playerInputActions.Player.Move;
+                bindingIndex=4;
+                break
+        }
+
+        inputAction.PerformInteractiveRebinding(bindingIndex)
+            .OnComplete(callback=>{
+                //callback.action.bindings[1];
+                callback.Dispose();
+                playerInputActions.Player.Enable();
+                onActionRebound();
+                PlayerPrefs.SetString(PLAYER_PREFS_BINDINGS,playerInputActions.SaveBindingOverridesAsJson());
+                PlayerPrefs.Save();
+
+                OnBindingRebind?.Invoke(this,EventArgs.Empty);
+            }).Start();
     }
 }
