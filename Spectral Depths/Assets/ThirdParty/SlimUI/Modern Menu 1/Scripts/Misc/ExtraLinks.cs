03864969a5a96b40bde2aec1f88d33d024488260
HaoranX39 <haoranxie2005@gmail.com>
2023-11-20 19:10:28 -0500

    Main Menu with Scene Transitions Added

---
+++ Spectral Depths/Assets/ThirdParty/SlimUI/Modern Menu 1/Scripts/Misc/ExtraLinks.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SlimUI.ModernMenu{
+    public class ExtraLinks : MonoBehaviour{
+        public void CCP(){
+            
+        }
+
+        public void SciFi(){
+        }
+
+        public void Clean1(){
+        }
+
+        public void Essence(){
+        }
+    }
+}
---
+++ Spectral Depths/Assets/ThirdParty/SlimUI/Modern Menu 1/Scripts/ThemeEditor/ThemedUIElement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace SlimUI.ModernMenu{
+	[System.Serializable]
+	public class ThemedUIElement : ThemedUI {
+		[Header("Parameters")]
+		Color outline;
+		Image image;
+		GameObject message;
+		public enum OutlineStyle {solidThin, solidThick, dottedThin, dottedThick};
+		public bool isImage = false;
+		public bool isBackground = false;
+		public bool isText = false;
+
+		protected override void OnSkinUI(){
+			base.OnSkinUI();
+
+			if(isImage){
+				image = GetComponent<Image>();
+				image.color = themeController.currentColor;
+			}
+			if(isBackground){
+				image = GetComponent<Image>();
+				image.color = themeController.currentBackground;
+			}
+
+			message = gameObject;
+
+			if(isText){
+				message.GetComponent<TextMeshPro>().color = themeController.textColor;
+			}
+		}
+	}
+}
